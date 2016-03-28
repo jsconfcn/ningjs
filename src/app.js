7f39b8676de4f15632a4f2bbcb91e3ab2900ecc2
z4rd <z4rd@live.cn>
2016-03-28 14:55:23 +0800

    Add site dev boilerplate by David.

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Header from 'Components/Header'
+
+ReactDOM.render(<Header />, document.querySelector('#app'))
---
+++ webpack.config.prod.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: [ './src/app.js' ],
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modulesDirectories: [
+      'src',
+      'node_modules',
+    ],
+    extensions: ['', '.json', '.js'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel'
+      }, {
+        test: /\.scss$/,
+        loader: 'style!css!autoprefixer?browsers=last 2 version!sass'
+      }, {
+        test: /\.css$/,
+        loader: 'style!css!autoprefixer?browsers=last 2 version'
+      }, {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      }, {
+        test: /\.json$/,
+        loader: 'json'
+      },
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false,
+        dead_code: true,
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        // Useful to reduce the size of client-side libraries, e.g. react
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+  ]
+}
